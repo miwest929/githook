f700e8a21d93b52b7afce63ba970223794c0387a
Michael West <mwest@mdsol.com>
2015-04-15 10:03:17 -0400

    Add some necessary specs

--- spec/githook_spec.rb
+++ spec/githook_spec.rb
@@ -4,11 +4,27 @@ describe Githook do
   context "registering new webhook action by invoking 'on'" do
     let(:mybot) { Githook::Bot.new }
 
-    it "successfully registers appropriate handler" do
+    it "successfully registers appropriate handler for the event#action specified" do
       handler = Proc.new {|pr| true}
       mybot.on('pull_request', ['opened'], &handler)
 
       expect(mybot.registry['pull_request-opened']).to eq(handler)
+      expect(mybot.registry['pull_request']).to eq(nil)
+    end
+
+    it "registers handler only for the event if no action is specified" do
+      handler = Proc.new {|pr| true}
+      mybot.on('pull_request', &handler)
+
+      expect(mybot.registry['pull_request']).to eq(handler)
+    end
+
+    it "registers handler only for each event#action  specified" do
+      handler = Proc.new {|pr| true}
+      mybot.on('pull_request', ['opened', 'reopened'], &handler)
+
+      expect(mybot.registry['pull_request-opened']).to eq(handler)
+      expect(mybot.registry['pull_request-reopened']).to eq(handler)
     end
   end
 end
